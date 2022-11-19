b7c314bfdf6d6ec5bd11ebdb8668937ec5cb13ff
Nicolaide Rares <rares.nicolaide@boots.co.uk>
2022-11-19 18:21:51 +0200

    fetched gist content in modal

--- src/components/GistContentModal.jsx
+++ src/components/GistContentModal.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 
 import "./GistContentModal.css";
 
 function GistContentModal({ setOpenFile, files, name }) {
+  const [content, setContent] = useState("");
+
   function close() {
     setOpenFile(null);
   }
+
+  useEffect(() => {
+    let ignore = false;
+    setContent("Loading...");
+    getContent(files[name].raw_url).then(
+      (result) => {
+        if (!ignore) {
+          setContent(result);
+        }
+      },
+      (error) => setContent(error)
+    );
+
+    return () => {
+      ignore = true;
+    };
+  }, [files, name]);
+
+  async function getContent(url) {
+    const res = await fetch(url);
+    return res.text();
+  }
+
   return createPortal(
     <>
       <div className="ModalShadow" onClick={close}></div>
       <div className="Modal">
         <div className="ModalContent">
           <div className="ModalHeader">{files[name].filename}</div>
+          <code>{content}</code>
         </div>
       </div>
     </>,
