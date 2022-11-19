1d265a0d40878ce71213285731bc6c5a79f49d60
Nicolaide Rares <rares.nicolaide@boots.co.uk>
2022-11-19 14:56:20 +0200

    mapped through user's gists and returned a Gist element for each

--- src/components/GistList.jsx
+++ src/components/GistList.jsx
@@ -1,10 +1,17 @@
 import React, { useContext } from "react";
+import Gist from "./Gist";
 import { GistContext } from "./GistTable";
 
 function GistList() {
   const [state] = useContext(GistContext);
 
-  return <div>GistList</div>;
+  return (
+    <div>
+      {state.gists.map((gist) => {
+        return <Gist key={gist.id} />;
+      })}
+    </div>
+  );
 }
 
 export default GistList;
