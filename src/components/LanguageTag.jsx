c5895845bad5443423d8c68c12750ec038864e47
Nicolaide Rares <rares.nicolaide@boots.co.uk>
2022-11-19 18:45:13 +0200

    added LanguageTag styling & function to generate tag color

--- src/components/LanguageTag.jsx
+++ src/components/LanguageTag.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+import "./LanguageTag.css";
+
 function LanguageTag({ language }) {
-  return <span>{language}</span>;
+  function generateColor(name) {
+    let hash = 0;
+    for (let i = 0; i < name.length; i++) {
+      hash = name.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let colour = "#";
+    for (let i = 0; i < 3; i++) {
+      const value = (hash >> (i * 8)) & 0xff;
+      colour += ("00" + value.toString(16)).substr(-2);
+    }
+    return colour;
+  }
+  const color = generateColor(language);
+  return (
+    <span style={{ backgroundColor: `${color}` }} className="tag">
+      {language}
+    </span>
+  );
 }
 
 export default LanguageTag;
