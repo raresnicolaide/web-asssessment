b65bc091d6a5133532d2bcc7a756e697814480d3
Nicolaide Rares <rares.nicolaide@boots.co.uk>
2022-11-19 13:37:15 +0200

    created gistReducer for GistTable state management

--- src/components/GistTable.jsx
+++ src/components/GistTable.jsx
@@ -1,4 +1,23 @@
-import React from "react";
+import React, { useReducer, createContext } from "react";
+
+const GistContext = createContext();
+
+function gistReducer(state, action) {
+  switch (action.type) {
+    case "IS_PENDING": {
+      return { ...state, isPending: true };
+    }
+    case "GIST_SUCCESS": {
+      return { ...state, gists: action.payload };
+    }
+    case "GIST_ERROR": {
+      return { ...state, error: action.payload };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
 
 function GistTable() {
   return <div>GistTable</div>;
