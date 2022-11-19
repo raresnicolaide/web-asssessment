dd0aeccab2c7f928e9cfdec6fcc4d77365ddd80d
Nicolaide Rares <rares.nicolaide@boots.co.uk>
2022-11-19 14:30:35 +0200

    consumed state from context in GistList

--- src/components/GistList.jsx
+++ src/components/GistList.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GistContext } from "./GistTable";
 
 function GistList() {
+  const [state] = useContext(GistContext);
+
   return <div>GistList</div>;
 }
 
