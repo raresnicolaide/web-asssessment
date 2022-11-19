2b189d5667950b7cbff6f4094828a1271e794272
Nicolaide Rares <rares.nicolaide@boots.co.uk>
2022-11-19 17:43:46 +0200

    added modal styling

--- src/components/GistContentModal.jsx
+++ src/components/GistContentModal.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { createPortal } from "react-dom";
 
+import "./GistContentModal.css";
+
 function GistContentModal({ setOpenFile, files, name }) {
   function close() {
     setOpenFile(null);
   }
   return createPortal(
     <>
-      <div onClick={close}></div>
-      <div>
-        <div>{files[name].filename}</div>
+      <div className="ModalShadow" onClick={close}></div>
+      <div className="Modal">
+        <div className="ModalContent">
+          <div className="ModalHeader">{files[name].filename}</div>
+        </div>
       </div>
     </>,
     document.getElementById("app-modal")
